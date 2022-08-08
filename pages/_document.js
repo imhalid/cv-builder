784d00330f8cd368397740de6d8e268be363acea
imhalid <imhalid@icloud.com>
2022-08-08 07:03:08 +0300

    almost finish layout

--- pages/_document.js
+++ pages/_document.js
@@ -4,8 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,wght@8..144,200;8..144,300;8..144,400;8..144,500;8..144,600&family=Spectral:ital,wght@1,500&display=swap"
           rel="stylesheet"
