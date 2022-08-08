b47a061e57859d659ff7501e888163a1f2368d5c
imhalid <imhalid@icloud.com>
2022-08-08 07:09:58 +0300

    almost finish layout

--- pages/_document.js
+++ pages/_document.js
@@ -2,19 +2,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,wght@8..144,200;8..144,300;8..144,400;8..144,500;8..144,600&family=Spectral:ital,wght@1,500&display=swap"
           rel="stylesheet"
         />
       </Head>
-      <body
-        className=" bg-neutral-100 h-screen
-       selection:bg-blue-100/30 selection:text-blue-500"
-      >
+      <body>
         <svg
           id="texture"
           width="100%"
