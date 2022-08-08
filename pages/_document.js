4f6c1dca7449b3ced8dacb43621b174a069c4113
imhalid <imhalid@icloud.com>
2022-08-08 07:19:34 +0300

    almost finish layout

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="icon" type="image/png" href="/assets/favicon.png" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Karla:wght@400;500;700&display=swap"
@@ -12,28 +11,6 @@ export default function Document() {
         />
       </Head>
       <body>
-        <svg
-          id="texture"
-          width="100%"
-          height="100%"
-          className="pointer-events-none fixed isolate z-50 opacity-40 mix-blend-soft-light"
-        >
-          <filter id="noise">
-            <feTurbulence
-              type="fractalNoise"
-              baseFrequency="0.4"
-              numOctaves="0.8"
-              stitchTiles="stitch"
-            ></feTurbulence>
-            //https://fecolormatrix.com/
-            <feColorMatrix
-              className="hidden dark:visible"
-              type="saturate"
-              values="0"
-            ></feColorMatrix>
-          </filter>
-          <rect width="100%" height="100%" filter="url(#noise)"></rect>
-        </svg>
         <Main />
         <NextScript />
       </body>
