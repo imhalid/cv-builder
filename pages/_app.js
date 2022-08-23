0a5217b56e1e597944c5a0f9b5c05b36b2f29dc8
imhalid <imhalid@icloud.com>
2022-08-23 21:08:29 +0300

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import Script from "next/script";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');`}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
